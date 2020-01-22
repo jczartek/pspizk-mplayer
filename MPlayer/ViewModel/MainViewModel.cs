bfc473b9edee1814baee097edf029e90fe580b1d
Jakub Czartek <jakubczartek@localhost.localdomain>
2020-01-22 14:08:49 +0100

    ...

---
+++ MPlayer/ViewModel/MainViewModel.cs
@@ -0,0 +1,34 @@
+﻿namespace MPlayer.ViewModel
+{
+    using MPlayer.Commands;
+    using System.Windows.Forms;
+    using System.Windows.Input;
+
+    class MainViewModel
+    {
+        private FolderBrowserDialog _folderBrowserDialog;
+        private RelayCommand _readFiles;
+        public ICommand ReadFiles
+        {
+            get
+            {
+                if (_readFiles == null)
+                {
+                    _readFiles = new RelayCommand(
+                        arg => 
+                        {
+                            if (_folderBrowserDialog == null)
+                                _folderBrowserDialog = new FolderBrowserDialog();
+
+                            var result = _folderBrowserDialog.ShowDialog();
+
+                            if (result == DialogResult.OK)
+                            {
+                            }
+                        });
+                }
+                return _readFiles;
+            }
+        }
+    }
+}
